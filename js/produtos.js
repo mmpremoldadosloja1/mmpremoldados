0d97e7b603d23aee11e03df34df4e271fec74bcb
mmpremoldadosloja1 <mmpremoldadosloja1@gmail.com>
2026-02-23 16:01:28 -0300

    Create produtos.js

---
+++ js/produtos.js
@@ -0,0 +1,58 @@
+const produtos = [
+
+  /* ================= COBÓGOS ================= */
+  {
+    nome: "Cobogó Anti-Chuva 40x40",
+    imagem: "img/cobogo-anti-chuva-40.jpg",
+    categoria: "cobogos",
+    destaque: true,
+    novidade: false
+  },
+  {
+    nome: "Cobogó 16 Furos 40x40",
+    imagem: "img/cobogo-16-furos-40.jpg",
+    categoria: "cobogos",
+    destaque: true,
+    novidade: false
+  },
+  {
+    nome: "Cobogó Flor 40x40",
+    imagem: "img/cobogo-flor-40.jpg",
+    categoria: "cobogos",
+    destaque: false,
+    novidade: true
+  },
+  {
+    nome: "Cobogó Estrela 30x30",
+    imagem: "img/cobogo-estrela-30.jpg",
+    categoria: "cobogos",
+    destaque: false,
+    novidade: true
+  },
+
+  /* ================= MANILHAS ================= */
+  {
+    nome: "Manilha 90cm x 50cm",
+    imagem: "img/manilhas/manilha-90x50.jpg",
+    categoria: "manilhas",
+    destaque: true,
+    novidade: false
+  },
+  {
+    nome: "Manilha 110cm x 50cm",
+    imagem: "img/manilhas/manilha-110x50.jpg",
+    categoria: "manilhas",
+    destaque: false,
+    novidade: false
+  },
+
+  /* ================= PINGADEIRAS ================= */
+  {
+    nome: "Pingadeira 1 Metro",
+    imagem: "img/pingadeiras/pingadeira-1m.jpg",
+    categoria: "pingadeiras",
+    destaque: true,
+    novidade: false
+  }
+
+];
